c6b419f59c4fd829b5decedc68bc9f748dbbf06b
Matthew Polasub <nondhawit@gmail.com>
2023-10-14 02:31:37 -0700

    ok cmon man

--- app/src/main/java/com/example/radiobuttons/MainActivity.java
+++ app/src/main/java/com/example/radiobuttons/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.radiobuttons;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
@@ -17,6 +18,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         radioGroup = (RadioGroup) findViewById(R.id.rgID);
-        radioGroup.setOnCheckedChangeListener();
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                radioButton = (RadioButton) findViewById(i);
+                switch (radioButton.getId()) {
+                    case R.id.yesID: {
+                        if(radioButton.isChecked()) { // if statement is optional
+                            Log.d("RD", "YES!");
+                        }
+                    }
+                    break;
+                    case R.id.noID: {
+                        if(radioButton.isChecked()) {
+                            Log.d("RD", "NO!");
+                        }
+                    }
+                    break;
+                    case R.id.maybeID: {
+                        if(radioButton.isChecked()) {
+                            Log.d("RD", "Maybe");
+                        }
+                    }
+                    break;
+                }
+            }
+        });
     }
 }
